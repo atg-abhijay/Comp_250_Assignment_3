964019dfcc736ab7df8b3fa71ddda95717439906
atg-abhijay <atg.abhijay@gmail.com>
2017-03-05 16:17:30 -0500

    modified 'cannot end with an operator' so that it doesn't throw exception if it ends with ( or ), changed name of operators stack from operatorsInOrder to nonNumeric, made new String Tokenizer to count number of +-*%

--- Expression.java
+++ Expression.java
@@ -20,11 +20,15 @@ public class Expression {
 	    } */
         
 	    /* YOU WRITE YOUR CODE HERE */
+
+        /* checking for the correct number
+           and formatting of parentheses */
         char[] exprAsChar = expr.toCharArray();
         boolean match = parenthMatching(exprAsChar);
         if (!match) {
             throw new IllegalArgumentException("The parentheses are not matched!");
         }
+
 		StringTokenizer st = new StringTokenizer(expr, delimiters, true);    
 		Stack<String> operators = new Stack<String>();
 		Stack<Integer> numbers = new Stack<Integer>();
@@ -47,7 +51,13 @@ public class Expression {
             if (!element.equals(")") && !element.equals("(")) {
                 countOperator++;
             }
-            if (st.countTokens() == 0) {
+            /* if there are no more tokens left after executing
+               this catch block and the last token just added 
+               is not a '(' or a ')', then that means that the
+               last token was one of +-*% (it can't be a number 
+               because we came into the catch block) and therefore
+               we throw an exception */
+            if (st.countTokens() == 0 && !element.equals(")") && !element.equals("(")) {
                 throw new IllegalArgumentException("The expression cannot end with an operator!");
             }
           }
@@ -58,13 +68,13 @@ public class Expression {
 
 	    }
 
-        Stack<String> operatorsInOrder = new Stack<String>();
+        Stack<String> nonNumeric = new Stack<String>();
         Stack<Integer> numbersInOrder = new Stack<Integer>();
         while(!numbers.empty()) {
             numbersInOrder.push(numbers.pop());
         }
         while(!operators.empty()) {
-            operatorsInOrder.push(operators.pop());
+            nonNumeric.push(operators.pop());
         }
 
         /* int i = 1;
@@ -75,36 +85,52 @@ public class Expression {
         
         System.out.println();
         int j = 1;
-        while(!operatorsInOrder.empty()) {
-              System.out.println(j + ". " + operatorsInOrder.pop());
+        while(!nonNumeric.empty()) {
+              System.out.println(j + ". " + nonNumeric.pop());
               j++;
         } */
         
-        //int operatorsSize = operatorsInOrder.size();
-        /* if the expression is only a single number
-           then we push a plus sign to the operators
-           stack because the number by itself cannot
-           be evaluated. e.g. 5 is written as 5+0. below
-           we push the number 0 to the numbers stack */
-        if (operatorsInOrder.size() == 0) {
-            operatorsInOrder.push("+");
+        //int operatorsSize = nonNumeric.size();
+
+        String otherDelimiters = "0123456789()";
+        StringTokenizer st2 = new StringTokenizer(expr, otherDelimiters, false);
+        int numOperations = st2.countTokens();
+        while(st2.hasMoreTokens()) {
+            System.out.print(st2.nextToken() + " ");
         }
-        System.out.println("Operators Size: " + operatorsInOrder.size());
+        System.out.println();
+        /* if the expression is only a single number
+           (without parentheses) then we push a plus 
+           sign to the operators stack because the number
+           by itself cannot be evaluated. e.g. 5 is 
+           written as 5+0. below we push the number 0 
+           to the numbers stack */
+        if (nonNumeric.size() == 0) {
+            nonNumeric.push("+");
+        }
+        System.out.println("Operators Size: " + nonNumeric.size());
         Integer answer = new Integer(0);
         Integer firstNum = numbersInOrder.peek();
         //while(!numbers.empty() || !operators.empty()) {
-        for(int i = 0; i < operatorsInOrder.size(); i++) {
+        for(int i = 0; i < nonNumeric.size(); i++) {
             System.out.print(firstNum + " ");
             if (i == 0) {
                 numbersInOrder.pop();
             }   
-            String operator = operatorsInOrder.peek();
+            String operator = nonNumeric.peek();
+            if (operator.equals("(")) {
+                nonNumeric.pop();
+                String nextOperator = nonNumeric.peek();
+                Stack<Integer> numbersWithin = new Stack<Integer>();
+                Stack<String> operatorsWithin = new Stack<String>();
+
+            }
             System.out.print(operator + " ");
-            operatorsInOrder.pop();
+            nonNumeric.pop();
             /* if the expression is a single number
-               we push a 0 so that it can act as
-               secondNum and the expr can now be
-               evaluated */
+               (without parentheses) we push a 0 so 
+               that it can act as secondNum and the 
+               expr can now be evaluated */
             if (numbersInOrder.empty()) {
                 numbersInOrder.push(0);
             }
