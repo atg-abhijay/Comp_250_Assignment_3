272c969c837efb6b925f86d316461fcc81d03ea8
atg-abhijay <atg.abhijay@gmail.com>
2017-03-07 17:44:57 -0500

    Final: added comments too

--- CardDeck.java
+++ CardDeck.java
@@ -34,16 +34,33 @@ class CardDeck {
     public void setupDeck(int n) {
 	/* WRITE YOUR CODE HERE */
 		Stack<Integer> firstN = new Stack<Integer>();
+		/* stack that stores the first
+		   n numbers in decreasing order
+		   (1 is at the bottom of the stack) */
 		for(int i = 1; i <= n; i++) {
 			Integer num = new Integer(i);
 			firstN.push(num);
 		}
+
+		/* if the deck contains nothing
+		   then we add the first (also the biggest)
+		   number from the stack to the deck */
 		if (this.deck.size() == 0) {
 			this.deck.addFirst(firstN.pop());
 		}
+
+		/* we perform the following operations
+		   until there are no more numbers left
+		   in the stack */
 		while(!firstN.empty()) {
+			/* we obtain the last 'card' in
+			   the deck and move it to the top
+			   of the deck */
 			Integer move = this.deck.pollLast();
 			this.deck.addFirst(move);
+			/* after the previous step, we add
+			   the number at the top of the stack
+			   to the top of the deck */
 			this.deck.addFirst(firstN.pop());
 		}
 
